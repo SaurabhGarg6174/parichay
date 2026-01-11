206daeae8b1910fc37da843be5044c60c5d19032
Saurabh Garg <saurabhgarg6174@gmail.com>
2026-01-12 00:34:35 +0530

    Initial commit

---
+++ src/main/java/com/aggarjan/patrika/parichay/ParichayApplication.java
@@ -0,0 +1,13 @@
+package com.aggarjan.patrika.parichay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParichayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParichayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aggarjan/patrika/parichay/ParichayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggarjan.patrika.parichay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParichayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
